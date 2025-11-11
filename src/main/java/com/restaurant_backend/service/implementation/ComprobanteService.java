41a9efca891969a2d2d75fb7e130d5f3ef89fbcf
Hernan21ZR <zamorahernan138@gmail.com>
2025-11-11 15:19:01 -0500

    Add-CORS

--- src/main/java/com/restaurant_backend/service/implementation/ComprobanteService.java
+++ src/main/java/com/restaurant_backend/service/implementation/ComprobanteService.java
@@ -1,19 +1,22 @@
 package com.restaurant_backend.service.implementation;
 
+import com.restaurant_backend.model.Comprobante;
 import com.restaurant_backend.model.DetallePedido;
+import com.restaurant_backend.repository.IComprobanteRepository;
 import com.restaurant_backend.repository.IDetallePedidoRepository;
 import com.restaurant_backend.repository.IGenericRepository;
+import com.restaurant_backend.service.IComprobanteService;
 import com.restaurant_backend.service.IDetallePedidoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ComprobanteService extends GenericService<DetallePedido, Integer> implements IDetallePedidoService {
-    private final IDetallePedidoRepository repo;
+public class ComprobanteService extends GenericService<Comprobante, Integer> implements IComprobanteService {
+    private final IComprobanteRepository repo;
 
     @Override
-    protected IGenericRepository<DetallePedido, Integer> getRepo() {
+    protected IGenericRepository<Comprobante, Integer> getRepo() {
         return repo;
     }
 }
